3e0b30168134db075d0ae5dc08b753f560d82346
Nikolai Pavlov <nik220822@rambler.ru>
2024-01-17 21:02:21 +0300

    JDBC problem

--- src/main/resources/db.changelog/migrations/dataUSERS.sql
+++ src/main/resources/db.changelog/migrations/dataUSERS.sql
@@ -2,4 +2,4 @@ insert into USERS (id, username, password)
 values
 ('id1','userone', '$2a$07$rfc7l4S5ZGXE68f/aNe4Bubrb0EQ3cOSAqevjyxUU0bNQMYlx55fO'),
 ('id2','usertwo', '$2a$07$6TzJ.nQltIaTg6vCD6IqSeYft.vS5ByDGu8n9ss57PUQHW7LYUGWO'),
-('id3','Nikolai', '$2a$07$zm3S4pnvxLALt/SRDnwYCOEa5/PPpeSDpaY5hAH2ZiLyFJQvs0.5u');
+('id3','nikolai', '$2a$12$CfmNQxtwOTgv5lUCSVmHse3UgrP.1vJKI5i0nMQ9JRgWnPZF5BmAK');
